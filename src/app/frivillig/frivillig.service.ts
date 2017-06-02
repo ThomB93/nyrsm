97092d8dc9df82f7bc19123cf23e7446cf65f793
Thomas <thomb@hotmail.dk>
2017-06-02 13:18:56 +0200

    Frivillig detaljer lavet

--- src/app/frivillig/frivillig.service.ts
+++ src/app/frivillig/frivillig.service.ts
@@ -8,6 +8,7 @@ export class FrivilligService {
   constructor() { }
   frivillige: Frivillig[] = [
     new Frivillig(1, 'Casper', 'Madsen', 'Jacob Gades Stræde 2', 7100, 'Vejle', 31574159, 'casp.mad@gmail.com', 'https://i.imgur.com/vQyC3DA.png'),
-    new Frivillig(2, 'Frants', 'Stentzer', 'Fredensgade 13', 6000, 'Kolding', 66666666, 'frants.ste@gmail.com', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVDTgtxoDw6nzJpdLUCnTI33HH2xuNi4sC6DCKNMLD-NHX_c1cRA')
+    new Frivillig(2, 'Frants', 'Stentzer', 'Fredensgade 13', 6000, 'Kolding', 66666666, 'frants.ste@gmail.com', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVDTgtxoDw6nzJpdLUCnTI33HH2xuNi4sC6DCKNMLD-NHX_c1cRA'),
+    
   ];
 }
